8c7373e8b68c4654f62670a7d5d728df9a32dceb
JeffMaigemu <jeffreysteven77@gmail.com>
2019-07-09 11:34:44 +0200

    fifth

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+$(document).ready(function(){
+    list = ["Kswahili","Hausa","Igbo","Yoruba","khosa","Berom","Tiv","Ngas","Igala","Ibibio","Taroh","African Languages"];
+    index = 0;
+    intr = setInterval(yourfun, 100);
+   
+    function yourfun()
+    { 
+       $("#languagesList").html(list[index++]);
+       if(index == 12) clearInterval(intr);
+    }
+
+    glowPulse("#languagesList", 0, 5, 100, 255, 255, 255);
+   });
+
 var email = document.getElementById("email");
 var submitBtn = document.getElementById("submitBtn");
 
@@ -9,3 +23,20 @@ function submitClick (){
 
     firebaseRef.child("email").set("Some Value");
 }
+
+function glowPulse(selector, min_radius, max_radius, speed, r, g, b, inc, radius){
+    
+    inc = inc == null ? 1 : inc;
+    radius = radius == null ? min_radius : radius;
+    
+    if(radius <= min_radius)
+        inc = 1;
+    else if(radius >= max_radius)
+        inc = -1;
+    
+    $(selector).css("text-shadow", "0px 0px "+radius+"px rgb("+r+","+g+","+b+")");
+    setTimeout("glowPulse('"+selector+"',"+[min_radius, max_radius, speed, r, g, b, inc, radius+inc].join(',')+")", speed);
+
+}
+
+
--- public/index.js
+++ public/index.js
@@ -1,9 +1,40 @@
+$(document).ready(function(){
+    list = ["Kswahili","Hausa","Igbo","Yoruba","khosa","Berom","Tiv","Ngas","Igala","Ibibio","Taroh","African Languages"];
+    index = 0;
+    intr = setInterval(yourfun, 100);
+   
+    function yourfun()
+    { 
+       $("#languagesList").html(list[index++]);
+       if(index == 12) clearInterval(intr);
+    }
+   });
+
 var email = document.getElementById("email");
 var submitBtn = document.getElementById("submitBtn");
 
 function submitClick (){
 
-    var firebaseRef = firebase.database().ref();
+    console.log("hi there, I was clicked");
+
+    let firebaseRef = this.database().ref();
 
     firebaseRef.child("email").set("Some Value");
 }
+
+function glowPulse(selector, min_radius, max_radius, speed, r, g, b, inc, radius){
+    
+    inc = inc == null ? 1 : inc;
+    radius = radius == null ? min_radius : radius;
+    
+    if(radius <= min_radius)
+        inc = 1;
+    else if(radius >= max_radius)
+        inc = -1;
+    
+    $(selector).css("text-shadow", "0px 0px "+radius+"px rgb("+r+","+g+","+b+")");
+    setTimeout("glowPulse('"+selector+"',"+[min_radius, max_radius, speed, r, g, b, inc, radius+inc].join(',')+")", speed);
+
+}
+
+glowPulse("#languagesList", 0, 5, 100, 255, 255, 255);
